ab23982fae99bf73b5395adb808af1a38d3a13e3
SergeyGolybtsov <kirsa200@gmail.com>
2023-03-05 17:17:09 +0300

    Сделал ДЗ

--- src/test/java/UserTest.java
+++ src/test/java/UserTest.java
@@ -1,2 +1,40 @@
-package PACKAGE_NAME;public class UserTest {
+import org.example.User;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+public class UserTest {
+    public static final String NORMAL_LOGIN = "login";
+    public static final String NORMAL_EMAIL = "email@mail.ru";
+    public static final String ILLEGAL_EMAIL = "email.ru";
+    public static final String LOGIN_EQUALS_EMAIL = "email@mail.ru";
+
+    @Test
+    @DisplayName("Пользователь успешно создан")
+    public void shouldCreateUserAllArgs() {
+        User user = new User(NORMAL_LOGIN, NORMAL_EMAIL);
+        assertEquals(NORMAL_LOGIN, user.getLogin());
+        assertEquals(NORMAL_EMAIL, user.getEmail());
+    }
+
+    @Test
+    @DisplayName("Пользователь успешно создан ")
+    public void shouldCreateUserNoArgs() {
+        User user = new User();
+        assertNull(user.getEmail());
+        assertNull(user.getLogin());
+    }
+
+    @Test
+    @DisplayName("Почта должна содержать \"@\" и \".\"")
+    public void isEmailValidCheck() {
+        assertThrows(IllegalArgumentException.class, () -> new User(NORMAL_LOGIN, ILLEGAL_EMAIL));
+    }
+
+    @Test
+    @DisplayName("Почта не должна совпадать с логином")
+    public void isEmailEqualsLogin() {
+        assertThrows(IllegalArgumentException.class, () -> new User(LOGIN_EQUALS_EMAIL, NORMAL_EMAIL));
+    }
 }
